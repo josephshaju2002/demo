11474bd1dce6dc739e6b622e3d6b64a10538de4e
josephshaju2002 <josephshaju466@gmail.com>
2025-10-25 20:55:33 +0530

    paragraph added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Hello</h1>
       <h1>Hello world </h1>
       <h2>Hello i am Bichu</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam exercitationem dolorem harum dignissimos aspernatur facere veritatis quos quas consequatur nesciunt corrupti incidunt itaque iure, ea eius veniam porro ab iste!</p>
     </>
   )
 }
